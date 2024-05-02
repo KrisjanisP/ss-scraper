b9906a7a002f01805295a373ad76fa7553da89fd
krisjanisp <krisjanispetrucena@gmail.com>
2024-05-02 09:58:36 +0300

    dont remember

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from download_html import download_all_pages
 from extract_data import extract_to_jsons
 import json
+import jinja2
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 root_path = os.path.dirname(script_dir)
@@ -17,11 +18,12 @@ price_lb = 3000
 price_ub = 12000
 year_lb = 2005
 year_ub = 2015
-power_lb = 200
+power_lb = 180
+power_ub = 500
 gearbox = "Automatic"
 
 all_cars = []
-for file in tqdm(os.listdir(os.path.join(dist_path, "JSON"))):
+for file in os.listdir(os.path.join(dist_path, "JSON")):
     with open(os.path.join(dist_path, "JSON", file), "r") as f:
         content = f.read()
         data = json.loads(content)
@@ -33,8 +35,19 @@ for file in tqdm(os.listdir(os.path.join(dist_path, "JSON"))):
                 continue
             if car["year"]>year_ub or car["year"]<year_lb:
                 continue
-            if car["power"]<power_lb:
+            if car["power"]<power_lb or car["power"]>power_ub:
                 continue
+            car["description"] = car["description"].strip()[:100].split("\n")[0]
+            car["ss_url"] = f"https://www.ss.lv/msg/lv/transport/cars/{key}.html"
             all_cars.append(car)
+# sort by price
+all_cars.sort(key=lambda x: x["price"])
 
-print(len(all_cars))
+# load jinja2 template
+templateLoader = jinja2.FileSystemLoader(searchpath=os.path.join(script_dir, "templates"))
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "table.html"
+template = templateEnv.get_template(TEMPLATE_FILE)
+outputText = template.render(cars=all_cars)
+with open(os.path.join(dist_path, "index.html"), "w") as f:
+    f.write(outputText)
